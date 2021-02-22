33b51494d11ee7afac8113f815ea8701438e7482
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2021-02-22 17:28:06 +0900

    Chnage: ExperimentMDDCWDetailのフィールドの型を変更

--- backend/domain/experiment_entity.go
+++ backend/domain/experiment_entity.go
@@ -23,8 +23,8 @@ type ExperimentMDDCWDetail struct {
 	ID                          int64
 	ExperimentID                int64
 	QuestplusParameterNormcdfID int64
-	Name                        string
-	Description                 string
+	Name                        ExperimentName
+	Description                 ExperimentDescription
 	Azimuth                     uint64 // 0[10^-1 deg] is the front, 900[10^-1 deg] is the right side
 	Altitude                    uint64 // 0[10^-1 deg] is the front, 900[10^-1 deg] is the zenith, -900[10^-1 deg] is the nadir
 	CoordinateVariableID        int64
