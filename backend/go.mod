7f52b490fb8222987b8a2934a23e3262a0c69805
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2021-02-28 19:27:07 +0900

    Change: 依存関係を変更

--- backend/go.mod
+++ backend/go.mod
@@ -7,5 +7,10 @@ require (
 	github.com/getkin/kin-openapi v0.39.0
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/jmoiron/sqlx v1.3.1
+	github.com/kr/pretty v0.2.0 // indirect
 	github.com/labstack/echo/v4 v4.2.0
+	github.com/lib/pq v1.9.0 // indirect
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
+	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
+	golang.org/x/sys v0.0.0-20210220050731-9a76102bfb43 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"os"
 
-	"github.com/tetsuzawa/web-spat/config"
 	"github.com/tetsuzawa/web-spat/interfaces/server"
-	"github.com/tetsuzawa/web-spat/interfaces/server/handler"
-	openapi "github.com/tetsuzawa/web-spat/interfaces/server/openapi"
 )
 
 func init() {
@@ -21,21 +17,5 @@ func main() {
 	var port int
 	flag.IntVar(&port, "port", 1991, "Web server port")
 	flag.Parse()
-
-	// connect to DB
-	db, err := config.NewDBConnection()
-	defer db.Close()
-	if err != nil {
-		log.Fatalln(fmt.Errorf("failed to init DB connection -> %w", err))
-	}
-
-	// routing
-	e := server.NewRouter()
-	h := handler.NewIntegratedHandler(
-		*handler.NewExperimentsHandler(),
-		*handler.NewUtilHandler(),
-	)
-	openapi.RegisterHandlersWithBaseURL(e, h, "/v1")
-
-	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", port)))
+	server.Run(port)
 }
