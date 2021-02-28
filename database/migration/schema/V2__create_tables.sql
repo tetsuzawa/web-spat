517af6c95545e544365335324a837292a560c9f0
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2021-02-28 16:37:02 +0900

    Change: スキーマをwidth, velocity兼用に変更

--- database/migration/schema/V2__create_tables.sql
+++ database/migration/schema/V2__create_tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `result_mddcw`
+CREATE TABLE IF NOT EXISTS `result_mdd`
 (
     `id`              INT PRIMARY KEY AUTO_INCREMENT,
     `experiment_id`   INT       NOT NULL,
@@ -9,59 +9,52 @@ CREATE TABLE IF NOT EXISTS `result_mddcw`
     `lower_asymptote` DOUBLE    NOT NULL,
     `lapse_rate`      DOUBLE    NOT NULL,
     `created_at`      TIMESTAMP NOT NULL DEFAULT (now())
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `experiment_mddcw`
+CREATE TABLE IF NOT EXISTS `experiment_mdd`
 (
     `id`         INT PRIMARY KEY AUTO_INCREMENT,
     `created_at` TIMESTAMP NOT NULL DEFAULT (now())
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `experiment_mddcw_active`
+CREATE TABLE IF NOT EXISTS `experiment_mdd_active`
 (
-    `id`            INT PRIMARY KEY AUTO_INCREMENT,
-    `experiment_id` INT UNIQUE NOT NULL,
-    `created_at`    TIMESTAMP  NOT NULL DEFAULT (now())
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    `experiment_id` INT PRIMARY KEY NOT NULL,
+    `created_at`    TIMESTAMP       NOT NULL DEFAULT (now())
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `experiment_mddcw_inactive`
+CREATE TABLE IF NOT EXISTS `experiment_mdd_inactive`
 (
-    `id`            INT PRIMARY KEY AUTO_INCREMENT,
-    `experiment_id` INT UNIQUE NOT NULL,
-    `created_at`    TIMESTAMP  NOT NULL DEFAULT (now())
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    `experiment_id` INT PRIMARY KEY NOT NULL,
+    `created_at`    TIMESTAMP       NOT NULL DEFAULT (now())
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `experiment_mddcw_detail`
+CREATE TABLE IF NOT EXISTS `experiment_mdd_detail`
 (
-    `id`                             INT PRIMARY KEY AUTO_INCREMENT,
-    `experiment_id`                  INT UNIQUE         NOT NULL,
-    `questplus_parameter_normcdf_id` INT                NOT NULL,
-    `name`                           varchar(64) UNIQUE NOT NULL,
-    `description`                    varchar(1024)      NOT NULL,
-    `azimuth`                        INT UNSIGNED       NOT NULL COMMENT '0[10^-1 deg] is the front, 900[10^-1 deg] is the right side',
-    `altitude`                       INT UNSIGNED       NOT NULL COMMENT '0[10^-1 deg] is the front, 900[10^-1 deg] is the zenith, -900[10^-1 deg] is the nadir',
-    `coordinate_variable_id`         INT                NOT NULL,
-    `width`                          INT UNSIGNED       NOT NULL COMMENT '[10^-1 deg]',
-    `velocity_range_lower`           INT UNSIGNED       NOT NULL COMMENT '[10^-1 deg/sec]',
-    `velocity_range_upper`           INT UNSIGNED       NOT NULL COMMENT '[10^-1 deg/sec]',
-    `velocity_range_step`            INT UNSIGNED       NOT NULL COMMENT '[10^-1 deg/sec]',
-    `num_trials`                     INT UNSIGNED       NOT NULL,
-    `created_at`                     TIMESTAMP          NOT NULL DEFAULT (now())
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    `experiment_id`                  INT PRIMARY KEY              NOT NULL,
+    `questplus_parameter_normcdf_id` INT                          NOT NULL,
+    `name`                           varchar(64) UNIQUE           NOT NULL,
+    `description`                    varchar(1024)                NOT NULL,
+    `azimuth`                        INT UNSIGNED                 NOT NULL COMMENT '0[10^-1 deg] is the front, 900[10^-1 deg] is the right side',
+    `altitude`                       INT UNSIGNED                 NOT NULL COMMENT '0[10^-1 deg] is the front, 900[10^-1 deg] is the zenith, -900[10^-1 deg] is the nadir',
+    `coordinate_variable`            ENUM ('azimuth', 'altitude') NOT NULL,
+    `moving_sound_variable`          ENUM ('width', 'velocity')   NOT NULL,
+    `num_trials`                     INT UNSIGNED                 NOT NULL,
+    `created_at`                     TIMESTAMP                    NOT NULL DEFAULT (now())
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `questplus_parameter_normcdf`
 (
     `id`                           INT PRIMARY KEY AUTO_INCREMENT,
     `questplus_parameter_json_url` TEXT      NOT NULL,
     `created_at`                   TIMESTAMP NOT NULL DEFAULT (now())
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS `m_coordinate_variable`
-(
-    `id`         INT PRIMARY KEY,
-    `type`       VARCHAR(64) NOT NULL COMMENT 'azimuth or altitude',
-    `created_at` TIMESTAMP   NOT NULL DEFAULT (now())
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `subject`
 (
@@ -70,25 +63,23 @@ CREATE TABLE IF NOT EXISTS `subject`
     `age`                       INT UNSIGNED NOT NULL,
     `deaf_and_hearing_impaired` BOOL         NOT NULL,
     `created_at`                TIMESTAMP    NOT NULL DEFAULT (now())
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
-ALTER TABLE `result_mddcw`
-    ADD FOREIGN KEY (`experiment_id`) REFERENCES `experiment_mddcw` (`id`);
+ALTER TABLE `result_mdd`
+    ADD FOREIGN KEY (`experiment_id`) REFERENCES `experiment_mdd` (`id`);
 
-ALTER TABLE `result_mddcw`
+ALTER TABLE `result_mdd`
     ADD FOREIGN KEY (`subject_id`) REFERENCES `subject` (`id`);
 
-ALTER TABLE `experiment_mddcw_active`
-    ADD FOREIGN KEY (`experiment_id`) REFERENCES `experiment_mddcw` (`id`);
+ALTER TABLE `experiment_mdd_active`
+    ADD FOREIGN KEY (`experiment_id`) REFERENCES `experiment_mdd` (`id`);
 
-ALTER TABLE `experiment_mddcw_inactive`
-    ADD FOREIGN KEY (`experiment_id`) REFERENCES `experiment_mddcw` (`id`);
+ALTER TABLE `experiment_mdd_inactive`
+    ADD FOREIGN KEY (`experiment_id`) REFERENCES `experiment_mdd` (`id`);
 
-ALTER TABLE `experiment_mddcw_detail`
-    ADD FOREIGN KEY (`experiment_id`) REFERENCES `experiment_mddcw` (`id`);
+ALTER TABLE `experiment_mdd_detail`
+    ADD FOREIGN KEY (`experiment_id`) REFERENCES `experiment_mdd` (`id`);
 
-ALTER TABLE `experiment_mddcw_detail`
+ALTER TABLE `experiment_mdd_detail`
     ADD FOREIGN KEY (`questplus_parameter_normcdf_id`) REFERENCES `questplus_parameter_normcdf` (`id`);
-
-ALTER TABLE `experiment_mddcw_detail`
-    ADD FOREIGN KEY (`coordinate_variable_id`) REFERENCES `m_coordinate_variable` (`id`);
