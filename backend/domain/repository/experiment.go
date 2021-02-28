aff1e1baadcf5803116530e93025dd77b13da751
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2021-02-28 19:25:03 +0900

    Fix: スキーマに合わせて関数名を変更

--- backend/domain/repository/experiment.go
+++ backend/domain/repository/experiment.go
@@ -2,12 +2,13 @@ package repository
 
 import (
 	"context"
+
 	"github.com/tetsuzawa/web-spat/domain"
 )
 
 // IExperimentRepository represent repository of the experiments.
 // Expect implementation by the infrastructure layer.
 type IExperimentRepository interface {
-	ListMDDCWActive(ctx context.Context) ([]*domain.ExperimentMDDCW, error)
-	ListMDDCWInactive(ctx context.Context) ([]*domain.ExperimentMDDCW, error)
+	ListMDDActive(ctx context.Context) ([]*domain.ExperimentMDD, error)
+	ListMDDInactive(ctx context.Context) ([]*domain.ExperimentMDD, error)
 }
